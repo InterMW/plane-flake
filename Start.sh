49f849360aa8f2ec34cc533814fb2e3702992a6e
Joseph-Melberg <joseph.melberg@myqops.com>
2022-03-14 23:32:02 -0500

    Easier than I thought

--- Start.sh
+++ Start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo $SOURCE is the source
 export LD_LIBRARY_PATH=/usr/local/lib/x86_64-linux-gnu && /dump1090-mutability/dump1090  \
 --rabbit-enable --rabbit-hostname rabbit.centurionx.local --rabbit-port 5672 \
---rabbit-exchange InterTopic --rabbit-routingkey plane.final --rabbit-user planey --rabbit-password mcplaneyface \
---rabbit-source aggregate --rabbit-antenna aggregate \
+--rabbit-exchange InterTopic --rabbit-routingkey plane.proto --rabbit-user planey --rabbit-password mcplaneyface \
+--rabbit-source $SOURCE --rabbit-antenna $ANTENNA \
 --write-json-every 1 --net-only --interactive  --lat 42.001 --lon -87.6595755 --phase-enhance \
 --oversample --interactive-rows 30 --max-range 600 --dcfilter --quiet 
 
