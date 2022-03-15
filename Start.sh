3e404a6f0de3051c69c4e8817cdb5d39201ad878
Joseph-Melberg <joseph.melberg@myqops.com>
2022-03-14 23:40:13 -0500

    day -1 bugs

--- Start.sh
+++ Start.sh
@@ -3,6 +3,6 @@ export LD_LIBRARY_PATH=/usr/local/lib/x86_64-linux-gnu && /dump1090-mutability/d
 --rabbit-enable --rabbit-hostname rabbit.centurionx.local --rabbit-port 5672 \
 --rabbit-exchange InterTopic --rabbit-routingkey plane.proto --rabbit-user planey --rabbit-password mcplaneyface \
 --rabbit-source $SOURCE --rabbit-antenna $ANTENNA \
---write-json-every 1 --net-only --interactive  --lat 42.001 --lon -87.6595755 --phase-enhance \
+--write-json-every 1 --interactive  --lat 42.001 --lon -87.6595755 --phase-enhance \
 --oversample --interactive-rows 30 --max-range 600 --dcfilter --quiet 
 
