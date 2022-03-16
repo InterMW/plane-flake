fc22f349409291b96eae6f5fcbbf0a21c5e3f794
Joseph-Melberg <dasbasdo@gmail.com>
2022-03-15 19:14:55 -0500

    Update StartNode.sh

--- StartNode.sh
+++ StartNode.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export LD_LIBRARY_PATH=/usr/local/lib/arm-linux-gnueabihf && /dump1090-mutability/dump1090  \
 --rabbit-enable --rabbit-hostname rabbit.centurionx.net --rabbit-port 5672 \
 --rabbit-exchange InterTopic --rabbit-routingkey plane.proto --rabbit-user planey --rabbit-password mcplaneyface \
