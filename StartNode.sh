7caa7ff61f59380da98fd6f764f742deaea24b5a
Joseph-Melberg <dasbasdo@gmail.com>
2022-03-24 21:39:15 +0000

    Complete

--- StartNode.sh
+++ StartNode.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo j > j
-echo $SOURCE > a
 export LD_LIBRARY_PATH=/usr/local/lib/arm-linux-gnueabihf && /dump1090-mutability/dump1090  \
 --rabbit-enable --rabbit-hostname rabbit.centurionx.net --rabbit-port 5672 \
---rabbit-exchange InterTopic --rabbit-routingkey plane.proto --rabbit-user planey --rabbit-password mcplaneyface \
+--rabbit-exchange InterTopic --rabbit-routingkey plane.proto --rabbit-user $RABBITUSER --rabbit-password $RABBITPASS \
 --rabbit-source $SOURCE --rabbit-antenna $ANTENNA \
---write-json-every 1 --net --interactive  --lat 42.001 --lon -87.6595755 --phase-enhance \
+--write-json-every 1 --net _-phase-enhance \
 --oversample --interactive-rows 30 --max-range 600 --dcfilter --quiet 
